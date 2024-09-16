f1a4e63ac7e72f2cc91f3e25beba296813128a86
vbayanag <147658931+vbayanag@users.noreply.github.com>
2024-09-16 15:49:44 -0700

    Update coco_captions.py with tokenizer field (currently unused) to enable training on COCO dataset

--- diffusion/datasets/coco/coco_captions.py
+++ diffusion/datasets/coco/coco_captions.py
@@ -101,6 +101,7 @@ def build_streaming_cocoval_dataloader(
     use_crop: bool = False,
     caption_selection='first',
     num_canonical_nodes: Optional[int] = None,
+    tokenizer: Optional[str] = None,
     **dataloader_kwargs,
 ):
     """Builds a streaming dataloader for the COCO validation set."""
